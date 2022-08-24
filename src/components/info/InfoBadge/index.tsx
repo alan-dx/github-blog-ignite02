8a04fab7459f3d6aaaee63aa663dab6e482ad5c3
Alan Almeida <alanalmeida.emp@gmail.com>
2022-08-24 11:22:07 -0300

    feat: create Profile and InfoBadge components

--- src/components/info/InfoBadge/index.tsx
+++ src/components/info/InfoBadge/index.tsx
@@ -7,7 +7,7 @@ interface IInfoBadge {
 
 export function InfoBadge({ icon, text }: IInfoBadge) {
   return (
-    <Flex align="center" gap="0.5rem" >
+    <Flex align="center" justify="center" gap="0.5rem" >
       <Icon as={icon} opacity="0.3" />
       <Text textStyle="text-m" as="span" color="base.subtitle">{text}</Text>
     </Flex>
--- src/components/info/Profile/index.tsx
+++ src/components/info/Profile/index.tsx
@@ -13,12 +13,12 @@ export function Profile() {
       boxShadow="0px 2px 28px rgba(0,0,0,0.2)"
     >
       <Image w="9.25rem" borderRadius="8px" src="https://www.github.com/alan-dx.png" alt="Foto de perfil do usuário" />
-      <Flex flexDir="column" justify="flex-end">
-        <Text as="h1" textStyle="title-l" color="base-title" >Alex Williamson</Text>
-        <Text textStyle="text-m" color="base.text" mt="0.625rem">
+      <Flex flexDir="column" justify={{ base: `flex-start`, md: 'flex-end' }}>
+        <Text as="h1" textStyle={["title-s", null, "title-l"]} color="base-title" >Alex Williamson</Text>
+        <Text display={{ base: `none`, md: 'block' }} textStyle="text-m" color="base.text" mt="0.625rem">
           Lorem nulla qui quis minim. Sit commodo reprehenderit eiusmod reprehenderit magna. Nisi ullamco anim elit exercitation elit qui cillum proident.
         </Text>
-        <Flex as="footer" gap="1.5rem" mt="1.5rem" >
+        <Flex as="footer" gap={{ base: `0.75rem`, md: '1.5rem' }} mt={["1rem", null, "1.5rem"]} wrap="wrap" >
           <InfoBadge icon={FaGithub} text="cammerrol" />
           <InfoBadge icon={FaBuilding} text="Rocketseat" />
           <InfoBadge icon={FaUserFriends} text="32 seguidores" />
